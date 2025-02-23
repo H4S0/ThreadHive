2f077408d40717294365d603fa97d697dd381dec
Hasan <87580658+H4S0@users.noreply.github.com>
2025-02-23 11:11:29 +0100

    Update route.ts

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -32,6 +32,6 @@ export async function GET() {
   return NextResponse.redirect(
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
-      : 'https://thread-hive-mocha.vercel.app'
+      : 'https://thread-hive-mocha.vercel.app/'
   );
 }
